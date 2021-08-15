185aec792c3400e834f57f8de50e974d27ea7652
feyeandal <andalfeye@gmail.com>
2021-08-15 18:19:51 +0800

    added new projects

--- data/projectsData.js
+++ data/projectsData.js
@@ -1,17 +1,37 @@
 const projectsData = [
   {
     title: 'HOT Philippines - PhilAWARE',
-    description: `HOT-Philippines started the PhilAWARE Project last July 2019. I led various remote and field mapping activities in Pampanga, Quezon City, and Marikina City.
+    description: `HOT-Philippines started the PhilAWARE Project last July 2019. I led various remote and field mapping operations in Pampanga, Quezon City, and Marikina City.
   `,
     imgSrc: '/static/images/hotph.png',
     href: 'https://feyeandal.github.io/hotphmappingteam/',
   },
   {
+    title: 'Development of a Web-based Awareness & Decision Support System (endcov.ph) on COVID-19',
+    description: `The web portal endcov.ph was released late of March 2020 to provide the UP community and the general public with vital information for fighting COVID-19 in the country.`,
+    imgSrc: '/static/images/endcov.png',
+    href: 'https://endcov.ph/',
+  },
+  {
     title: 'Building Resilient Communities in Bohol',
-    description: `This project built the building footprints and road network and produced municipal-level risk maps in the province of Bohol using open-source tools such as OpenStreetMap (OSM), QGIS, and WebSAFE that can be used for disaster risk planning and mitigation of the province.`,
+    description: `Through this project, the UP Resilience Institute Youth Mappers mapped the building footprints and road network and produced municipal-level risk maps in the province of Bohol using open-source tools such as OpenStreetMap (OSM), QGIS, and WebSAFE that can be used for disaster risk planning and mitigation of the province.`,
     imgSrc: '/static/images/bohol_ym.jpg',
     href:
       'https://wiki.openstreetmap.org/wiki/Philippines/Building_Resilient_Communities_in_Bohol_Through_the_Use_of_OpenStreetMap',
+  },
+  {
+    title: 'Project NOAH - ISAIAH ',
+    description: `Project NOAH has finished mapping the building footprints of ISAIAH’s 15 target provinces through OpenStreetMap. The translated building footprints data mapped by NOAH through OSM is integrated into the WebSAFE application of Project NOAH.`,
+    imgSrc: '/static/images/PH_ISAIAH.gif',
+    href: 'https://www.openstreetmap.org/user/feyeandal/diary/40567',
+  },
+  {
+    title:
+      'Community Mapping for Disaster Risk Reduction and Management: Harnessing Local Knowledge to Build Resilience',
+    description: `In 2013, the World Bank, the Department of Interior and Local Government, and the Institute of Environmental Science for Social Change launched a project on Community Mapping and LGU Decision Support Tools for Disaster Risk Reduction and Management.`,
+    imgSrc: '/static/images/essc_osm.png',
+    href:
+      'https://www.gfdrr.org/sites/default/files/publication/Community-Mapping-for-Disaster-Risk-Reduction-and-Management_0.pdf',
   },
 ]
 
