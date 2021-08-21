c3d3565846ba58ba856974c35d4eeb6207af27ea
feyeandal <andalfeye@gmail.com>
2021-08-21 19:13:43 +0800

    added new maps

--- data/mapsData.js
+++ data/mapsData.js
@@ -6,13 +6,49 @@ const mapsData = [
     imgSrc: '/static/images/Cavite_preview.gif',
   },
   {
-    imgSrc: '/static/images/typhoon.jpg',
+    imgSrc: '/static/images/worst_typhoons.jpg',
   },
   {
-    imgSrc: '/static/images/schools.jpg',
+    imgSrc: '/static/images/ph_publicschools.jpg',
   },
   {
-    imgSrc: '/static/images/volcano.jpg',
+    imgSrc: '/static/images/dangerzone.jpg',
+  },
+  {
+    imgSrc: '/static/images/gps.jpg',
+  },
+  {
+    imgSrc: '/static/images/us_buildings.jpg',
+  },
+  {
+    imgSrc: '/static/images/bohol_landslide.jpg',
+  },
+  {
+    imgSrc: '/static/images/ph_elevation.jpg',
+  },
+  {
+    imgSrc: '/static/images/world_aqi.jpg',
+  },
+  {
+    imgSrc: '/static/images/ph_brgys.jpg',
+  },
+  {
+    imgSrc: '/static/images/world_timezones.jpg',
+  },
+  {
+    imgSrc: '/static/images/ph_urbanbrgys.jpg',
+  },
+  {
+    imgSrc: '/static/images/ph_forestcover.jpg',
+  },
+  {
+    imgSrc: '/static/images/ph_temp.jpg',
+  },
+  {
+    imgSrc: '/static/images/ncr_flood.jpg',
+  },
+  {
+    imgSrc: '/static/images/ph_bathymetry.jpg',
   },
 ]
 
