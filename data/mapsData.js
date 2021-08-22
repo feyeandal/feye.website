0a9e87ea93c5eb4515afd6326970d7e597e8f2df
feyeandal <andalfeye@gmail.com>
2021-08-22 08:49:27 +0800

    description to maps

--- data/mapsData.js
+++ data/mapsData.js
@@ -1,71 +1,43 @@
 const mapsData = [
   {
     imgSrc: '/static/images/Pampanga_animated_v2.gif',
-    title: 'HOT Philippines PhilAWARE - Pampanga',
+    title: "Pampanga's OSM Building Footprints and Road Network",
   },
   {
     imgSrc: '/static/images/Cavite_preview.gif',
-    title: 'HOT Philippines PhilAWARE - Pampanga',
+    title: "Cavite's OSM Building Footprints",
   },
   {
     imgSrc: '/static/images/worst_typhoons.jpg',
-    title: 'HOT Philippines PhilAWARE - Pampanga',
+    title: 'Worst Typhoons in the Philippines',
   },
   {
-    imgSrc: '/static/images/ph_publicschools.jpg',
-    title: 'HOT Philippines PhilAWARE - Pampanga',
+    imgSrc: '/static/images/elevation.jpg',
+    title: "San Mateo, Rizal's Elevation  in Lego Style",
   },
   {
     imgSrc: '/static/images/dangerzone.jpg',
-    title: 'HOT Philippines PhilAWARE - Pampanga',
-  },
-  {
-    imgSrc: '/static/images/gps.jpg',
-    title: 'HOT Philippines PhilAWARE - Pampanga',
+    title: "Mt. Pinatubos's Danger Zone",
   },
   {
     imgSrc: '/static/images/us_buildings.jpg',
-    title: 'HOT Philippines PhilAWARE - Pampanga',
+    title: "New York's OSM Building Footprints",
   },
   {
     imgSrc: '/static/images/bohol_landslide.jpg',
-    title: 'HOT Philippines PhilAWARE - Pampanga',
-  },
-  {
-    imgSrc: '/static/images/ph_elevation.jpg',
-    title: 'HOT Philippines PhilAWARE - Pampanga',
+    title: 'Landslide Hazard in Bohol',
   },
   {
     imgSrc: '/static/images/world_aqi.jpg',
-    title: 'HOT Philippines PhilAWARE - Pampanga',
-  },
-  {
-    imgSrc: '/static/images/ph_brgys.jpg',
-    title: 'HOT Philippines PhilAWARE - Pampanga',
+    title: "World's Air Quality Index",
   },
   {
     imgSrc: '/static/images/world_timezones.jpg',
-    title: 'HOT Philippines PhilAWARE - Pampanga',
-  },
-  {
-    imgSrc: '/static/images/ph_urbanbrgys.jpg',
-    title: 'HOT Philippines PhilAWARE - Pampanga',
-  },
-  {
-    imgSrc: '/static/images/ph_forestcover.jpg',
-    title: 'HOT Philippines PhilAWARE - Pampanga',
+    title: 'Timezones of the World',
   },
   {
     imgSrc: '/static/images/ph_temp.jpg',
-    title: 'HOT Philippines PhilAWARE - Pampanga',
-  },
-  {
-    imgSrc: '/static/images/ncr_flood.jpg',
-    title: 'HOT Philippines PhilAWARE - Pampanga',
-  },
-  {
-    imgSrc: '/static/images/ph_bathymetry.jpg',
-    title: 'HOT Philippines PhilAWARE - Pampanga',
+    title: 'Land Surface Temperature Anomaly',
   },
 ]
 
