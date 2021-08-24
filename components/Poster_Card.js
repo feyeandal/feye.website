876b757332acc4b32200eb8beabe8f0a318ccd67
feyeandal <andalfeye@gmail.com>
2021-08-24 09:58:31 +0800

    clean code on postercard

--- components/Poster_Card.js
+++ components/Poster_Card.js
@@ -23,27 +23,6 @@ const Card = ({ title, description, imgSrc, href }) => (
           height={950}
         />
       )}
-      {/* <div className="p-6">
-        <h2 className="mb-3 text-3xl font-bold leading-8 tracking-tight">
-          {href ? (
-            <Link href={href} aria-label={`Link to ${title}`}>
-              {title}
-            </Link>
-          ) : (
-            title
-          )}
-        </h2>
-        <p className="mb-3 prose text-gray-500 max-w-none dark:text-gray-400">{description}</p>
-        {href && (
-          <Link
-            href={href}
-            className="text-base font-medium leading-6 text-primary-500 hover:text-primary-600 dark:hover:text-primary-400"
-            aria-label={`Link to ${title}`}
-          >
-            Learn more &rarr;
-          </Link>
-        )}
-      </div> */}
     </div>
   </div>
 )
