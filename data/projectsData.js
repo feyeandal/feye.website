648613024bfabbfa7e4eeda00abf1fcc0bbcca01
feyeandal <andalfeye@gmail.com>
2021-08-24 17:28:33 +0800

    small changes on the description

--- data/projectsData.js
+++ data/projectsData.js
@@ -1,8 +1,7 @@
 const projectsData = [
   {
     title: 'HOT Philippines - PhilAWARE',
-    description: `HOT-Philippines started the PhilAWARE Project last July 2019. I led various remote and field mapping operations in Pampanga, Quezon City, and Marikina City.
-  `,
+    description: `HOT-Philippines, together with the Pacific Disaster Center (PDC), Office of Civil Defense, PetaBencana.id, and USAID started the PhilAWARE Project last July 2019. The project aims to collect the local critical lifeline infrastructures based on the PhilAWARE Data Model and complete the building footprints and road network of Pampanga, Quezon City, and Marikina City.`,
     imgSrc: '/static/images/hotph.png',
     href: 'https://feyeandal.github.io/hotphmappingteam/',
   },
@@ -14,14 +13,14 @@ const projectsData = [
   },
   {
     title: 'Building Resilient Communities in Bohol',
-    description: `Through this project, the UP Resilience Institute Youth Mappers mapped the building footprints and road network and produced municipal-level risk maps in the province of Bohol using open-source tools such as OpenStreetMap (OSM), QGIS, and WebSAFE that can be used for disaster risk planning and mitigation of the province.`,
+    description: `The UP Resilience Institute Youth Mappers (UPRIYM) was awarded a grant by the TELUS Community Board to implement this project. Building footprints and road network were mapped and municipal-level risk maps were produced for the province of Bohol using open-source tools such as OpenStreetMap (OSM), QGIS, and WebSAFE that can be used for disaster risk planning and mitigation of the province.`,
     imgSrc: '/static/images/bohol_ym.jpg',
     href:
       'https://wiki.openstreetmap.org/wiki/Philippines/Building_Resilient_Communities_in_Bohol_Through_the_Use_of_OpenStreetMap',
   },
   {
     title: 'Project NOAH - ISAIAH ',
-    description: `Project NOAH has finished mapping the building footprints of ISAIAH’s 15 target provinces through OpenStreetMap. The translated building footprints data mapped by NOAH through OSM is integrated into the WebSAFE application of Project NOAH.`,
+    description: `Project NOAH finished mapping the building footprints of ISAIAH’s 15 target provinces through OpenStreetMap. The translated building footprints data mapped by NOAH through OSM is integrated into the WebSAFE application of Project NOAH.`,
     imgSrc: '/static/images/PH_ISAIAH.gif',
     href: 'https://www.openstreetmap.org/user/feyeandal/diary/40567',
   },
