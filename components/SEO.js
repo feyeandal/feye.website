a7b2140f25dcdfda6446b09fd3d38ba917a70d56
feyeandal <andalfeye@gmail.com>
2022-01-15 10:48:48 +0800

    test social banner

--- components/SEO.js
+++ components/SEO.js
@@ -14,12 +14,12 @@ export const PageSeo = ({ title, description }) => {
       <meta property="og:site_name" content={siteMetadata.title} />
       <meta property="og:description" content={description} />
       <meta property="og:title" content={title} />
-      {/* <meta property="og:image" content={`${siteMetadata.siteUrl}${siteMetadata.socialBanner}`} /> */}
+      <meta property="og:image" content={`${siteMetadata.siteUrl}${siteMetadata.socialBanner}`} />
       <meta name="twitter:card" content="summary_large_image" />
       <meta name="twitter:site" content={siteMetadata.twitter} />
       <meta name="twitter:title" content={title} />
       <meta name="twitter:description" content={description} />
-      {/* <meta name="twitter:image" content={`${siteMetadata.siteUrl}${siteMetadata.socialBanner}`} /> */}
+      <meta name="twitter:image" content={`${siteMetadata.siteUrl}${siteMetadata.socialBanner}`} />
     </Head>
   )
 }
