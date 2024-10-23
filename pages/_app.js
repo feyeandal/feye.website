694c2b694fdeacc2c9c516e9f9e9f750246d7aa7
feyeandal <andalfeye@gmail.com>
2024-10-23 10:04:52 +0800

    add analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,8 @@
 import '@/css/tailwind.css'
-
 import { ThemeProvider } from 'next-themes'
 import Head from 'next/head'
-
 import LayoutWrapper from '@/components/LayoutWrapper'
+import { Analytics } from '@vercel/analytics/react'
 
 export default function App({ Component, pageProps }) {
   return (
@@ -14,6 +13,7 @@ export default function App({ Component, pageProps }) {
       <LayoutWrapper>
         <Component {...pageProps} />
       </LayoutWrapper>
+      <Analytics /> {/* Add this line to enable Vercel Analytics */}
     </ThemeProvider>
   )
 }
